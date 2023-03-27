a142c43f1310078cf0a969b113b07001ca139e25
gerikov <gerikov.mining@gmail.com>
2023-03-27 15:49:40 +0200

    basic server init

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-console.log("server running");
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+// middlewares
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
