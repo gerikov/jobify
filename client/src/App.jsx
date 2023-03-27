cfd5e8b46a7a249f389d5ae72295c898359e0948
gerikov <gerikov.mining@gmail.com>
2023-03-27 09:42:57 +0200

    jobify init

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "normalize.css";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1>jobify</h1>;
+}
+
+export default App;
