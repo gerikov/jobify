c4922e8dc5841c40ecde596ee40c7bd370c8d26f
gerikov <gerikov.mining@gmail.com>
2023-03-30 10:52:34 +0200

    adding sidebars

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,9 +1,30 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h1>small sidebar</h1>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
